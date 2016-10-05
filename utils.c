ae49be45072f484fc626a11756adc712414f81fb
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2016-10-05 00:32:09 -0300

    improved utils.h

--- utils.c
+++ utils.c
@@ -1,15 +1,65 @@
 #include "utils.h"
 
-int isNumber(char* str)
+int isDecimal1024(char* str)
 {
-    long value = strtol(str, NULL, 0);
-    if (!value)
-        for (int i = 0 ; i < strlen(str); ++i)
-        {
-            if (*(str + i) != '0' && (*(str + i) != 'x' && i == 1))
-                return 0;
-        }
-    return 1;
+    regex_t regex;
+
+    regcomp(&regex, "^[0-9]{1,4}$", REG_EXTENDED|REG_NOSUB);
+    int retorno = !regexec(&regex, str, 0, NULL, 0);
+    regfree(&regex);
+    if (retorno)
+    {
+        long value = strtol(str, NULL, 0);
+        if (0 <= value && value < 1024)
+            return 1;
+    }
+
+    return 0;
+}
+
+int isDecimal(char* str)
+{
+    regex_t regex;
+
+    regcomp(&regex, "^[0-9]{1,10}$", REG_EXTENDED|REG_NOSUB);
+    int retorno = !regexec(&regex, str, 0, NULL, 0);
+    regfree(&regex);
+    if (retorno)
+    {
+        long value = strtol(str, NULL, 0);
+        if (0 <= value && value < MAXINT)
+            return 1;
+    }
+
+    return 0;
+}
+
+int isDecimalNegative(char* str)
+{
+    regex_t regex;
+
+    regcomp(&regex, "^-?[0-9]{1,32}$", REG_EXTENDED|REG_NOSUB);
+    int retorno = !regexec(&regex, str, 0, NULL, 0);
+    regfree(&regex);
+    if (retorno)
+    {
+        long value = strtol(str, NULL, 0);
+        if (-MAXINT <= value && value < MAXINT)
+            return 1;
+    }
+
+    return 0;
+}
+
+int isHexadecimalNumber(char* str)
+{
+    regex_t regex;
+
+    regcomp(&regex, "^0x[0-9a-fA-F]{10}$", REG_EXTENDED|REG_NOSUB);
+    int retorno = !regexec(&regex, str, 0, NULL, 0);
+    regfree(&regex);
+
+    return retorno;
 }
 
 int isLabel(char* str)
@@ -30,6 +80,29 @@ int isSymbol(char* str)
 
     /*Verifica se eh formada por caracteres e '_', simbolo*/
     regcomp(&regex, "^[a-z_A-Z]+$", REG_EXTENDED|REG_NOSUB);
+    int retorno = !regexec(&regex, str, 0, NULL, 0);
+    regfree(&regex);
+
+    return retorno;
+}
+
+int isComment(char* str)
+{
+    regex_t regex;
+
+    /*Verifica se comeca com '#', um comentario*/
+    regcomp(&regex, "^#", REG_EXTENDED|REG_NOSUB);
+    int retorno = !regexec(&regex, str, 0, NULL, 0);
+    regfree(&regex);
+
+    return retorno;
+}
+
+int isDirective(char* str)
+{
+    regex_t regex;
+
+    regcomp(&regex, "^\\.[a-z]+$", REG_EXTENDED|REG_NOSUB);
     int retorno = !regexec(&regex, str, 0, NULL, 0);
     regfree(&regex);
 
--- utils.h
+++ utils.h
@@ -3,9 +3,14 @@
 #include <ctype.h>
 #include <regex.h>
 
+#define MAXINT 2147483647
+
 /*Checa se o parametro eh um numero ou nao (decimal ou hexadecimal)*/
-int isNumber(char* str);
-
+int isDecimal1024(char* str);
+int isDecimal(char* str);
+int isDecimalNegative(char* str);
+int isHexadecimalNumber(char* str);
 int isLabel(char* str);
-
 int isSymbol(char* str);
+int isComment(char* str);
+int isDirective(char* str);
